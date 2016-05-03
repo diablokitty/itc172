2ee038259576ad46c5a24600c660d9e18a2ec842
diablokitty <diablokitty@gmail.com>
2016-05-03 11:28:15 -0700

    files for sample service client

---
+++ sampleserviceclient/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Author Sample Service</title>
+</head>
+<body>
+    <h1>Get Books By Author</h1>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="authorDropDownList" runat="server"></asp:DropDownList>
+        <asp:GridView ID="bookGridView" runat="server"></asp:GridView>
+        <asp:Button ID="BookButton" runat="server" Text="Get Books" OnClick="BookButton_Click"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ sampleserviceclient/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    samplebookreviewservicereference.SampleBookReviewServiceClient bsc = new samplebookreviewservicereference.SampleBookReviewServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+            FillAuthorDropDown();
+    }
+    protected void FillAuthorDropDown()
+    {
+        string[] authors = bsc.GetAuthors();
+        authorDropDownList.DataSource = authors;
+        authorDropDownList.DataBind();
+    }
+    protected void BookButton_Click(object sender, EventArgs e)
+    {
+        string author = authorDropDownList.SelectedItem.Text;
+        samplebookreviewservicereference.BookLite[] books = bsc.GetBooks(author);
+        bookGridView.DataSource = books;
+        bookGridView.DataBind();
+     }
+
+
+
+    protected void BookButton_Click1(object sender, EventArgs e)
+    {
+
+    }
+}
